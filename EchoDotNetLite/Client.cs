d8e92cbf842e2a0223314a56199d109b0195d80d
smdn <smdn@smdn.jp>
2023-11-30 21:42:36 +0900

    Fix conditional expression intended to determine whether a DEOJ is specified or not

--- EchoDotNetLite/Client.cs
+++ EchoDotNetLite/Client.cs
@@ -1179,7 +1179,7 @@ namespace EchoDotNetLite
         {
             bool hasError = false;
             var opcList = new List<PropertyRequest>();
-            if (destObject != null)
+            if (destObject == null)
             {
                 //DEOJがない場合、全OPCをそのまま返す
                 hasError = true;
@@ -1257,7 +1257,7 @@ namespace EchoDotNetLite
         {
             bool hasError = false;
             var opcList = new List<PropertyRequest>();
-            if (destObject != null)
+            if (destObject == null)
             {
                 //DEOJがない場合、全OPCをそのまま返す
                 hasError = true;
@@ -1336,7 +1336,7 @@ namespace EchoDotNetLite
             bool hasError = false;
             var opcSetList = new List<PropertyRequest>();
             var opcGetList = new List<PropertyRequest>();
-            if (destObject != null)
+            if (destObject == null)
             {
                 //DEOJがない場合、全OPCをそのまま返す
                 hasError = true;
