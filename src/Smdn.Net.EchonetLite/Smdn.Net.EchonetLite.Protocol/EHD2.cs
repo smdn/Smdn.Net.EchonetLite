01020a2e501f150c14623346e9395123b9b40f42
smdn <smdn@smdn.jp>
2024-04-03 22:21:09 +0900

    improve XML comment docs and fix warnings

--- src/Smdn.Net.EchonetLite/Smdn.Net.EchonetLite.Protocol/EHD2.cs
+++ src/Smdn.Net.EchonetLite/Smdn.Net.EchonetLite.Protocol/EHD2.cs
@@ -3,16 +3,26 @@
 // SPDX-License-Identifier: MIT
 namespace Smdn.Net.EchonetLite.Protocol;
 
+/// <summary>
+/// ECHONET Lite ヘッダ2 (EDH2)の値を表す列挙体です。
+/// ECHONET Lite フレームのEDATA部の電文形式を規定します。
+/// </summary>
+/// <seealso href="https://echonet.jp/spec_v114_lite/">
+/// ECHONET Lite規格書 Ver.1.14 第2部 ECHONET Lite 通信ミドルウェア仕様 ３.２.１.１ ECHONET Lite ヘッダ２（EHD２）
+/// </seealso>
+/// <seealso href="https://echonet.jp/spec_v114_lite/">
+/// ECHONET Lite規格書 Ver.1.14 第2部 ECHONET Lite 通信ミドルウェア仕様 図 ３-３ EHD2 詳細規定
+/// </seealso>
+#pragma warning disable CA1008
 public enum EHD2 : byte {
-  // 図 ３-３ EHD2 詳細規定
+#pragma warning restore CA1008
   /// <summary>
-  /// 形式1
+  /// EDATA部の電文形式として「電文形式 1（規定電文形式）」を表す値を示します。
   /// </summary>
-  Type1 = 0x81,
+  Type1 = 0b_1000_0001,
+
   /// <summary>
-  /// 形式2
+  /// EDATA部の電文形式として「電文形式 2（任意電文形式）」を表す値を示します。
   /// </summary>
-  Type2 = 0x82,
-  // その他:future reserved
-  // ただし、b7=1固定
+  Type2 = 0b_1000_0010,
 }
