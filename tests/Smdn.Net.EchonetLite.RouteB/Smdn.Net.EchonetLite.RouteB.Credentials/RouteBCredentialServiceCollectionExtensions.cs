354c38ccedb790ca69a3c88b3eefd20bec4ec124
smdn <smdn@smdn.jp>
2024-06-30 20:23:57 +0900

    add test project for Smdn.Net.EchonetLite.RouteB

---
+++ tests/Smdn.Net.EchonetLite.RouteB/Smdn.Net.EchonetLite.RouteB.Credentials/RouteBCredentialServiceCollectionExtensions.cs
@@ -0,0 +1,94 @@
+// SPDX-FileCopyrightText: 2024 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+using System;
+using System.Buffers;
+using System.Text;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using NUnit.Framework;
+
+namespace Smdn.Net.EchonetLite.RouteB.Credentials;
+
+[TestFixture]
+public class RouteBCredentialServiceCollectionExtensions {
+  private const string ID = "00112233445566778899AABBCCDDEEFF";
+  private const string Password = "0123456789AB";
+
+  private static (string ID, string Password) GetCredential(
+    IRouteBCredentialProvider provider,
+    IRouteBCredentialIdentity identity
+  )
+  {
+    using var credential = provider.GetCredential(identity: identity);
+
+    var bufferForID = new ArrayBufferWriter<byte>();
+    var bufferForPassword = new ArrayBufferWriter<byte>();
+
+    credential.WriteIdTo(bufferForID);
+    credential.WritePasswordTo(bufferForPassword);
+
+    var id = Encoding.ASCII.GetString(bufferForID.WrittenSpan);
+    var password = Encoding.ASCII.GetString(bufferForPassword.WrittenSpan);
+
+    return (id, password);
+  }
+
+  [Test]
+  public void AddRouteBCredential()
+  {
+    var services = new ServiceCollection();
+
+    services.AddRouteBCredential(
+      id: ID,
+      password: Password
+    );
+
+    var credentialProvider = services.BuildServiceProvider().GetRequiredService<IRouteBCredentialProvider>();
+
+    Assert.That(credentialProvider, Is.Not.Null, nameof(credentialProvider));
+
+    var (id, password) = GetCredential(credentialProvider, identity: null!);
+
+    Assert.That(id, Is.EqualTo(ID));
+    Assert.That(password, Is.EqualTo(Password));
+  }
+
+  [Test]
+  public void AddRouteBCredential_TryAddMultiple()
+  {
+    var services = new ServiceCollection();
+
+    services.AddRouteBCredential(
+      id: ID,
+      password: Password
+    );
+    services.AddRouteBCredential(
+      id: "this must not be selected",
+      password: "this must not be selected"
+    );
+
+    var credentialProvider = services.BuildServiceProvider().GetRequiredService<IRouteBCredentialProvider>();
+
+    Assert.That(credentialProvider, Is.Not.Null, nameof(credentialProvider));
+
+    var (id, password) = GetCredential(credentialProvider, identity: null!);
+
+    Assert.That(id, Is.EqualTo(ID));
+    Assert.That(password, Is.EqualTo(Password));
+  }
+
+  [TestCase(ID, null)]
+  [TestCase(null, Password)]
+  public void AddRouteBCredential_ArgumentNull(string? id, string? password)
+  {
+    var services = new ServiceCollection();
+
+    Assert.Throws<ArgumentNullException>(
+      () => services.AddRouteBCredential(
+        id: id!,
+        password: password!
+      )
+    );
+  }
+}
