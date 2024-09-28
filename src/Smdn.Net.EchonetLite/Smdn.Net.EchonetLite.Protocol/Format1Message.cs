2c1d7847edf2fee9626ea09867339d9bc4cf9b8b
smdn <smdn@smdn.jp>
2024-09-28 12:57:34 +0900

    improve xml comment docs

--- src/Smdn.Net.EchonetLite/Smdn.Net.EchonetLite.Protocol/Format1Message.cs
+++ src/Smdn.Net.EchonetLite/Smdn.Net.EchonetLite.Protocol/Format1Message.cs
@@ -13,7 +13,7 @@ using Smdn.Net.EchonetLite.Serialization.Json;
 namespace Smdn.Net.EchonetLite.Protocol;
 
 /// <summary>
-/// ECHONET Liteフレームにおける、電文形式 1（規定電文形式）のEDATA(ECHONET Lite データ)を表す読み取り専用の構造体です。
+/// ECHONET Liteフレームにおける、電文形式 1（規定電文形式/specified message format）のEDATA(ECHONET Lite データ)を表す読み取り専用の構造体です。
 /// </summary>
 /// <seealso href="https://echonet.jp/spec_v114_lite/">
 /// ECHONET Lite規格書 Ver.1.14 第2部 ECHONET Lite 通信ミドルウェア仕様 第３章 電文構成（フレームフォーマット）
@@ -23,19 +23,27 @@ namespace Smdn.Net.EchonetLite.Protocol;
 /// </seealso>
 public readonly struct Format1Message {
   /// <summary>
-  /// 送信元ECHONET Liteオブジェクト指定(3B)
+  /// 送信元ECHONET Liteオブジェクト指定(SEOJ)を表す<see cref="EOJ"/>を取得します。
   /// </summary>
+  /// <seealso href="https://echonet.jp/spec_v114_lite/">
+  /// ECHONET Lite規格書 Ver.1.14 第2部 ECHONET Lite 通信ミドルウェア仕様 ３．２ 電文構成
+  /// </seealso>
   public EOJ SEOJ { get; }
 
   /// <summary>
-  /// 相手先ECHONET Liteオブジェクト指定(3B)
+  /// 相手先ECHONET Liteオブジェクト指定(DEOJ)を表す<see cref="EOJ"/>を取得します。
   /// </summary>
+  /// <seealso href="https://echonet.jp/spec_v114_lite/">
+  /// ECHONET Lite規格書 Ver.1.14 第2部 ECHONET Lite 通信ミドルウェア仕様 ３．２ 電文構成
+  /// </seealso>
   public EOJ DEOJ { get; }
 
   /// <summary>
-  /// ECHONET Liteサービス(1B)
-  /// ECHONET Liteサービスコード
+  /// ECHONET Liteサービス(ESV)を表す<see cref="ESV"/>を取得します。
   /// </summary>
+  /// <seealso href="https://echonet.jp/spec_v114_lite/">
+  /// ECHONET Lite規格書 Ver.1.14 第2部 ECHONET Lite 通信ミドルウェア仕様 ３．２ 電文構成
+  /// </seealso>
   [JsonConverter(typeof(SingleByteJsonConverterFactory))]
   public ESV ESV { get; }
 
@@ -87,7 +95,7 @@ public readonly struct Format1Message {
   /// ECHONET Liteフレームの電文形式 1（規定電文形式）の電文を記述する<see cref="Format1Message"/>を作成します。
   /// </summary>
   /// <remarks>
-  /// このオーバーロードでは、<paramref name="esv"/>が<see cref="ESV.SetGet"/>, <see cref="ESV.SetGetResponse"/>, <see cref="ESV.SetGetServiceNotAvailable"/>のいずれかではない場合に例外をスローします。
+  /// このオーバーロードでは、<paramref name="esv"/>が<see cref="ESV.SetGet"/>, <see cref="ESV.SetGetResponse"/>, <see cref="ESV.SetGetServiceNotAvailable"/>のいずれでもない場合に例外をスローします。
   /// </remarks>
   /// <param name="seoj"><see cref="SEOJ"/>に指定する値。</param>
   /// <param name="deoj"><see cref="DEOJ"/>に指定する値。</param>
@@ -111,6 +119,17 @@ public readonly struct Format1Message {
     this.opcGetList = opcGetList ?? throw new ArgumentNullException(nameof(opcGetList));
   }
 
+  /// <summary>
+  /// <see cref="ESV"/>で指定されるECHONET Liteサービスにおける、処理対象となるプロパティのコレクションを取得します。
+  /// このメソッドの戻り値は、電文形式 1（規定電文形式）における処理プロパティ数(OPC)・ECHONET Liteプロパティ(EPC)・EDTのバイト数(PDC)・プロパティ値データ(EDT)の部分を表現します。
+  /// </summary>
+  /// <exception cref="InvalidOperationException">
+  /// <see cref="ESV"/>が<see cref="ESV.SetGet"/>, <see cref="ESV.SetGetResponse"/>, <see cref="ESV.SetGetServiceNotAvailable"/>のいずれかです。
+  /// 代わりに<see cref="GetOPCSetGetList"/>を呼び出してください。
+  /// </exception>
+  /// <seealso href="https://echonet.jp/spec_v114_lite/">
+  /// ECHONET Lite規格書 Ver.1.14 第2部 ECHONET Lite 通信ミドルウェア仕様 ３．２ 電文構成
+  /// </seealso>
   public IReadOnlyCollection<PropertyRequest> GetOPCList()
   {
     if (IsWriteOrReadService)
@@ -123,6 +142,17 @@ public readonly struct Format1Message {
 #endif
   }
 
+  /// <summary>
+  /// <see cref="ESV"/>で指定されるECHONET Liteサービスにおける、Set操作・Get操作の処理対象となるプロパティのコレクションを取得します。
+  /// このメソッドの戻り値は、電文形式 1（規定電文形式）における処理プロパティ数(OPC)・ECHONET Liteプロパティ(EPC)・EDTのバイト数(PDC)・プロパティ値データ(EDT)の部分を表現します。
+  /// </summary>
+  /// <exception cref="InvalidOperationException">
+  /// <see cref="ESV"/>が<see cref="ESV.SetGet"/>, <see cref="ESV.SetGetResponse"/>, <see cref="ESV.SetGetServiceNotAvailable"/>のいずれでもありません。
+  /// 代わりに<see cref="GetOPCList"/>を呼び出してください。
+  /// </exception>
+  /// <seealso href="https://echonet.jp/spec_v114_lite/">
+  /// ECHONET Lite規格書 Ver.1.14 第2部 ECHONET Lite 通信ミドルウェア仕様 ３．２ 電文構成
+  /// </seealso>
   public (
     IReadOnlyCollection<PropertyRequest> OPCSetList,
     IReadOnlyCollection<PropertyRequest> OPCGetList
