31eaec142b538a8c20f03eaa9833e18da88ebec2
smdn <smdn@smdn.jp>
2024-09-29 14:04:32 +0900

    add test cases

--- tests/Smdn.Net.EchonetLite/Smdn.Net.EchonetLite.Protocol/PropertyValue.cs
+++ tests/Smdn.Net.EchonetLite/Smdn.Net.EchonetLite.Protocol/PropertyValue.cs
@@ -1,10 +1,40 @@
 // SPDX-FileCopyrightText: 2023 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
+using System;
+
 using NUnit.Framework;
 
 namespace Smdn.Net.EchonetLite.Protocol;
 
 [TestFixture]
 public class PropertyValueTests {
+  [Test]
+  public void Default()
+  {
+    var p = default(PropertyValue);
+
+    Assert.That(p.EPC, Is.EqualTo(0x00));
+    Assert.That(p.PDC, Is.EqualTo(0));
+    Assert.That(p.EDT.IsEmpty, Is.True);
+  }
+
+  [Test]
+  public void Ctor_Empty()
+  {
+    var p = new PropertyValue(0x00, ReadOnlyMemory<byte>.Empty);
+
+    Assert.That(p.EPC, Is.EqualTo(0x00));
+    Assert.That(p.PDC, Is.EqualTo(0));
+    Assert.That(p.EDT.IsEmpty, Is.True);
+  }
+
+  [Test]
+  public void Ctor_ArgumentException()
+  {
+    Assert.That(
+      () => new PropertyValue(0x00, new byte[0x100]),
+      Throws.ArgumentException
+    );
+  }
 }
 
