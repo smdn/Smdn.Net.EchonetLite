d2dbc17dc6a203186da6fb97d9dd3335d2e0300e
smdn <smdn@smdn.jp>
2024-10-04 21:13:57 +0900

    fix xml comment docs

--- src/Smdn.Net.EchonetLite/Smdn.Net.EchonetLite/EchonetClient.ServiceHandlings.cs
+++ src/Smdn.Net.EchonetLite/Smdn.Net.EchonetLite/EchonetClient.ServiceHandlings.cs
@@ -339,7 +339,7 @@ partial class EchonetClient
   /// <see cref="Task{T}.Result"/>には処理の結果が含まれます。
   /// 要求を正常に処理した場合は<see langword="true"/>、そうでなければ<see langword="false"/>が設定されます。
   /// </returns>
-  /// <seealso cref="PerformPropertyValueReadRequestAsync"/>
+  /// <seealso cref="PerformPropertyValueReadRequestAsync(EchonetObject, EchonetNode?, EchonetObject, IEnumerable{PropertyValue}, CancellationToken)"/>
   /// <seealso href="https://echonet.jp/spec_v114_lite/">
   /// ECHONET Lite規格書 Ver.1.14 第2部 ECHONET Lite 通信ミドルウェア仕様 ３．２．５ ECHONET Lite サービス（ESV）
   /// </seealso>
