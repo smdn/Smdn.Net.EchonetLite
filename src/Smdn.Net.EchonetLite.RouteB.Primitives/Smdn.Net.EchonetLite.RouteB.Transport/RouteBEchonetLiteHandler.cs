5a1e616f92f11381be00dfda08763a40b39b7b5e
smdn <smdn@smdn.jp>
2024-11-06 22:06:30 +0900

    fix to call StopReceivingAsync only if receiving task is running

--- src/Smdn.Net.EchonetLite.RouteB.Primitives/Smdn.Net.EchonetLite.RouteB.Transport/RouteBEchonetLiteHandler.cs
+++ src/Smdn.Net.EchonetLite.RouteB.Primitives/Smdn.Net.EchonetLite.RouteB.Transport/RouteBEchonetLiteHandler.cs
@@ -73,7 +73,8 @@ public abstract class RouteBEchonetLiteHandler : EchonetLiteHandler {
         cancellationToken: cancellationToken
       ).ConfigureAwait(false);
 
-      await StopReceivingAsync().ConfigureAwait(false);
+      if (IsReceiving)
+        await StopReceivingAsync().ConfigureAwait(false);
     }
   }
 
