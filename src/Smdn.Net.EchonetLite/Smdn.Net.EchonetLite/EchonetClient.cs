a9f05bafd92a308d05bc92394e080dc435e095b2
smdn <smdn@smdn.jp>
2024-11-23 14:21:46 +0900

    fix warning IDE0005

--- src/Smdn.Net.EchonetLite/Smdn.Net.EchonetLite/EchonetClient.cs
+++ src/Smdn.Net.EchonetLite/Smdn.Net.EchonetLite/EchonetClient.cs
@@ -8,7 +8,9 @@ using System;
 using System.Net;
 using System.Threading.Tasks;
 
+#if SYSTEM_TIMEPROVIDER
 using Microsoft.Extensions.DependencyInjection;
+#endif
 using Microsoft.Extensions.Logging;
 
 using Polly;
