df0f6fba69e7642ca24a723742335c78dfc13a12
smdn <smdn@smdn.jp>
2024-12-04 21:22:40 +0900

    reimplement UdpEchonetLiteHandler

--- src/Smdn.Net.EchonetLite/Smdn.Net.EchonetLite.Transport/UdpEchonetLiteHandler.cs
+++ src/Smdn.Net.EchonetLite/Smdn.Net.EchonetLite.Transport/UdpEchonetLiteHandler.cs
@@ -5,27 +5,56 @@
 
 using System;
 using System.Buffers;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Smdn.Net.EchonetLite.Transport;
 
 public class UdpEchonetLiteHandler : EchonetLiteHandler {
-  private UdpClient? receiveUdpClient;
-  private readonly IReadOnlyList<IPAddress> selfAddresses;
-  private const int DefaultUdpPort = 3610;
+  // > ECHONET Lite規格書 Ver.1.14 第2部 ECHONET Lite 通信ミドルウェア仕様 １．２ 通信レイヤ上の位置づけ
+  // > （１）Layer4 で UDP(User Datagram Protocol)、Layer3 で IP(Internet Protocol)、を使用する場合
+  // > IPv4 の場合、送信先マルチキャストアドレス値は 224.0.23.0 とする。
+  // > また、ネットワーク内に存在する ECHONET Lite ノードを発見するために、
+  // > ノード発見用メッセージを一斉同報送信する際には IP ブロードキャストを併用しても良い。
+  // > ただし、ブロードキャストを使用する際は、必ずマルチキャストと併用することとする。
+  private static readonly IPEndPoint IPv4MulticastEndPoint = new(IPAddress.Parse("224.0.23.0"), DefaultPort);
+
+  // > ECHONET Lite規格書 Ver.1.14 第2部 ECHONET Lite 通信ミドルウェア仕様 １．２ 通信レイヤ上の位置づけ
+  // > （１）Layer4 で UDP(User Datagram Protocol)、Layer3 で IP(Internet Protocol)、を使用する場合
+  // > IPv6 の場合、ff02::1（オールノードマルチキャストアドレス）を用いるものとする。
+  // > ただし、IPv4、IPv6 のいずれの場合も、OSI 参照モデル 4 層以下の仕様を
+  // > 他の規格団体が定めている仕様に準拠する場合は、該当する規格団体が定める
+  // > マルチキャストアドレスを使用する。
+  private static readonly IPEndPoint IPv6MulticastEndPoint = new(IPAddress.Parse("ff02::1"), DefaultPort);
+
+  private IPEndPoint LocalEndPoint { get; }
+  private IPEndPoint MulticastEndPoint { get; }
 
   /// <inheritdoc/>
-  public override IPAddress? LocalAddress => throw new NotSupportedException(); // TODO
+  public override IPAddress? LocalAddress => LocalEndPoint.Address;
+
+  private UdpClient? sendClient;
+  private UdpClient? receiveClient;
 
   public UdpEchonetLiteHandler(
+    IPAddress localAddress,
+    IServiceProvider? serviceProvider = null
+  )
+    : this(
+      localAddress: localAddress ?? throw new ArgumentNullException(nameof(localAddress)),
+      logger: serviceProvider?.GetService<ILoggerFactory>()?.CreateLogger<UdpEchonetLiteHandler>(),
+      serviceProvider: serviceProvider
+    )
+  {
+  }
+
+  public UdpEchonetLiteHandler(
+    IPAddress localAddress,
     ILogger? logger,
     IServiceProvider? serviceProvider
   )
@@ -34,55 +63,45 @@ public class UdpEchonetLiteHandler : EchonetLiteHandler {
       serviceProvider
     )
   {
-    selfAddresses = NetworkInterface.GetAllNetworkInterfaces().SelectMany(ni => ni.GetIPProperties().UnicastAddresses.Select(ua => ua.Address)).ToArray();
+    LocalEndPoint = new(
+      address: localAddress ?? throw new ArgumentNullException(nameof(localAddress)),
+      port: DefaultPort // TODO: make configurable
+    );
 
-    try {
-      receiveUdpClient = new UdpClient(DefaultUdpPort) {
-        EnableBroadcast = true,
-      };
-    }
-    catch (Exception ex) {
-      logger?.LogError(ex, $"unexpected exception occured while initialization");
-      throw;
-    }
+    MulticastEndPoint = LocalEndPoint.AddressFamily switch {
+      AddressFamily.InterNetwork => IPv4MulticastEndPoint,
+      AddressFamily.InterNetworkV6 => IPv6MulticastEndPoint,
+      _ => throw new ArgumentException("local address must be an address of IPv4 or IPv6", nameof(localAddress)),
+    };
+
+    StartReceiving();
   }
 
+  /// <inheritdoc/>
   protected override void Dispose(bool disposing)
   {
-    if (disposing) {
-      try {
-        receiveUdpClient?.Close();
-        receiveUdpClient?.Dispose();
-        receiveUdpClient = null;
-      }
-#pragma warning disable CA1031
-      catch (Exception ex) {
-        Logger?.LogWarning(ex, $"unexpected exception occured while disposing {nameof(UdpClient)} for receiving");
-
-        // swallow all exceptions
-      }
-#pragma warning restore CA1031
-    }
-
     base.Dispose(disposing);
+
+    DisposeCore();
   }
 
-  protected override ValueTask DisposeAsyncCore()
+  /// <inheritdoc/>
+  protected override async ValueTask DisposeAsyncCore()
   {
-    try {
-      receiveUdpClient?.Close();
-      receiveUdpClient?.Dispose();
-      receiveUdpClient = null;
-    }
-#pragma warning disable CA1031
-    catch (Exception ex) {
-      Logger?.LogWarning(ex, $"unexpected exception occured while disposing {nameof(UdpClient)} for receiving");
+    await base.DisposeAsyncCore().ConfigureAwait(false);
 
-      // swallow all exceptions
-    }
-#pragma warning restore CA1031
+    DisposeCore();
+  }
 
-    return base.DisposeAsyncCore();
+  private void DisposeCore()
+  {
+    sendClient?.Close();
+    sendClient?.Dispose();
+    sendClient = null;
+
+    receiveClient?.Close();
+    receiveClient?.Dispose();
+    receiveClient = null;
   }
 
   /// <inheritdoc/>
@@ -91,13 +110,25 @@ public class UdpEchonetLiteHandler : EchonetLiteHandler {
     CancellationToken cancellationToken
   )
   {
+    ThrowIfDisposed();
+
     if (buffer is null)
       throw new ArgumentNullException(nameof(buffer));
-    if (receiveUdpClient is null)
-      throw new ObjectDisposedException(GetType().FullName);
+
+    try {
+      receiveClient ??= new UdpClient(LocalEndPoint);
+    }
+    catch (Exception ex) {
+      Logger?.LogError(
+        ex,
+        $"unexpected exception occured while initializing {nameof(UdpClient)} {{LocalEndPoint}}",
+        LocalEndPoint
+      );
+      throw;
+    }
 
     for (; ; ) {
-      var receivedResults = await receiveUdpClient
+      var receivedResults = await receiveClient
 #if SYSTEM_NET_SOCKETS_UDPCLIENT_RECEIVEASYNC_CANCELLATIONTOKEN
         .ReceiveAsync(cancellationToken)
 #else
@@ -105,13 +136,13 @@ public class UdpEchonetLiteHandler : EchonetLiteHandler {
 #endif
         .ConfigureAwait(false);
 
-      if (selfAddresses.Contains(receivedResults.RemoteEndPoint.Address))
+      if (LocalEndPoint.Address.Equals(receivedResults.RemoteEndPoint.Address))
         // ブロードキャストを自分で受信した(無視)
         continue;
 
       Logger?.LogTrace(
-        "UDP receive from {Address}: {Buffer}",
-        receivedResults.RemoteEndPoint.Address,
+        "UDP receive from {RemoteEndPoint}: {Buffer}",
+        receivedResults.RemoteEndPoint,
         ((ReadOnlyMemory<byte>)receivedResults.Buffer).ToHexString()
       );
 
@@ -123,8 +154,8 @@ public class UdpEchonetLiteHandler : EchonetLiteHandler {
 
   private void LogSend(IPEndPoint remoteEndPoint, ReadOnlyMemory<byte> buffer)
     => Logger?.LogTrace(
-      "UDP send to {Address}: {Buffer}",
-      remoteEndPoint.Address,
+      "UDP send to {RemoteEndPoint}: {Buffer}",
+      remoteEndPoint,
       buffer.ToHexString()
     );
 
@@ -134,23 +165,29 @@ public class UdpEchonetLiteHandler : EchonetLiteHandler {
     CancellationToken cancellationToken
   )
   {
-    var remoteEndPoint = new IPEndPoint(IPAddress.Broadcast, DefaultUdpPort);
+    ThrowIfDisposed();
 
-    LogSend(remoteEndPoint, buffer);
+    LogSend(MulticastEndPoint, buffer);
 
-    using var udpClient = new UdpClient() {
-      EnableBroadcast = true,
-    };
-
-    udpClient.Connect(remoteEndPoint);
+    try {
+      sendClient ??= new() {
+        EnableBroadcast = true,
+      };
+    }
+    catch (Exception ex) {
+      Logger?.LogError(
+        ex,
+        $"unexpected exception occured while initializing {nameof(UdpClient)} {{LocalEndPoint}}",
+        LocalEndPoint
+      );
+      throw;
+    }
 
 #if SYSTEM_NET_SOCKETS_UDPCLIENT_SENDASYNC_READONLYMEMORY_OF_BYTE
-    await udpClient.SendAsync(buffer, cancellationToken).ConfigureAwait(false);
+    await sendClient.SendAsync(buffer, MulticastEndPoint, cancellationToken).ConfigureAwait(false);
 #else
-    await udpClient.SendAsync(buffer.ToArray(), buffer.Length).ConfigureAwait(false);
+    await sendClient.SendAsync(buffer.ToArray(), buffer.Length, MulticastEndPoint).ConfigureAwait(false);
 #endif
-
-    udpClient.Close();
   }
 
   /// <inheritdoc/>
@@ -160,20 +197,20 @@ public class UdpEchonetLiteHandler : EchonetLiteHandler {
     CancellationToken cancellationToken
   )
   {
-    var remoteEndPoint = new IPEndPoint(remoteAddress, DefaultUdpPort); // TODO: reduce allocation
+    ThrowIfDisposed();
+
+    var remoteEndPoint = new IPEndPoint(remoteAddress, DefaultPort); // TODO: reduce allocation
 
     LogSend(remoteEndPoint, buffer);
 
-    using var udpClient = new UdpClient();
-
-    udpClient.Connect(remoteEndPoint);
+    sendClient ??= new() {
+      EnableBroadcast = true,
+    };
 
 #if SYSTEM_NET_SOCKETS_UDPCLIENT_SENDASYNC_READONLYMEMORY_OF_BYTE
-    await udpClient.SendAsync(buffer, cancellationToken).ConfigureAwait(false);
+    await sendClient.SendAsync(buffer, remoteEndPoint, cancellationToken).ConfigureAwait(false);
 #else
-    await udpClient.SendAsync(buffer.ToArray(), buffer.Length).ConfigureAwait(false);
+    await sendClient.SendAsync(buffer.ToArray(), buffer.Length, remoteEndPoint).ConfigureAwait(false);
 #endif
-
-    udpClient.Close();
   }
 }
