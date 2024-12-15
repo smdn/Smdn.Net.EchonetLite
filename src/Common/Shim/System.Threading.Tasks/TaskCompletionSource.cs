cc4399821a92e5feca55dc1b693381b02ab0c594
smdn <smdn@smdn.jp>
2024-12-15 13:55:37 +0900

    bump Smdn.MSBuild.DefineConstants.NETSdkApi up to 1.5.1

--- src/Common/Shim/System.Threading.Tasks/TaskCompletionSource.cs
+++ src/Common/Shim/System.Threading.Tasks/TaskCompletionSource.cs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 #pragma warning disable CA1812
 
-#if !NET5_0_OR_GREATER
+#if !SYSTEM_THREADING_TASKS_TASKCOMPLETIONSOURCE
 namespace System.Threading.Tasks;
 
 internal class TaskCompletionSource : TaskCompletionSource<TaskCompletionSource.Void> {
