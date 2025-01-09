6fce950aa9b1d79888ddea780d74804b373a2963
smdn <smdn@smdn.jp>
2025-01-10 02:05:30 +0900

    add tests

---
+++ tests/Smdn.Net.EchonetLite.RouteB/Smdn.Net.EchonetLite.RouteB/ElectricCurrentValue.cs
@@ -0,0 +1,70 @@
+// SPDX-FileCopyrightText: 2025 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+using System;
+using NUnit.Framework;
+
+namespace Smdn.Net.EchonetLite.RouteB;
+
+[TestFixture]
+public class ElectricCurrentValueTests {
+  [TestCase(0x_0000)]
+  [TestCase(0x_0001)]
+  [TestCase(0x_7FFD)]
+  [TestCase(0x_8001)]
+  [TestCase(0x_FFFF)]
+  [TestCase(0x_7FFE)]
+  [TestCase(0x_7FFF)]
+  [TestCase(0x_8000)]
+  public void RawValue(int rawValue)
+  {
+    var value = new ElectricCurrentValue((short)rawValue);
+    var expected = (short)rawValue;
+
+    Assert.That(value.RawValue, Is.EqualTo(expected));
+  }
+
+  [TestCase(0x_0000, true)]
+  [TestCase(0x_0001, true)]
+  [TestCase(0x_7FFD, true)]
+  [TestCase(0x_8001, true)]
+  [TestCase(0x_FFFF, true)]
+  [TestCase(0x_7FFE, false)]
+  [TestCase(0x_7FFF, false)]
+  [TestCase(0x_8000, false)]
+  public void IsValid(int rawValue, bool expected)
+  {
+    var value = new ElectricCurrentValue((short)rawValue);
+
+    Assert.That(value.IsValid, Is.EqualTo(expected));
+  }
+
+  [TestCase(0x_0000, 0.0)]
+  [TestCase(0x_0001, 0.1)]
+  [TestCase(0x_7FFD, 3276.5)]
+  [TestCase(0x_8001, -3276.7)]
+  [TestCase(0x_FFFF, -0.1)]
+  [TestCase(0x_7FFE, 0.0)] // invalid
+  [TestCase(0x_7FFF, 0.0)] // invalid
+  [TestCase(0x_8000, 0.0)] // invalid
+  public void Amperes(int rawValue, decimal expected)
+  {
+    var value = new ElectricCurrentValue((short)rawValue);
+
+    Assert.That(value.Amperes, Is.EqualTo(expected));
+  }
+
+  [TestCase(0x_0000, "0.0 [A]")]
+  [TestCase(0x_0001, "0.1 [A]")]
+  [TestCase(0x_7FFD, "3276.5 [A]")]
+  [TestCase(0x_8001, "-3276.7 [A]")]
+  [TestCase(0x_FFFF, "-0.1 [A]")]
+  [TestCase(0x_7FFE, "(no data)")]
+  [TestCase(0x_7FFF, "(overflow)")]
+  [TestCase(0x_8000, "(underflow)")]
+  public void ToString(int rawValue, string expected)
+  {
+    var value = new ElectricCurrentValue((short)rawValue);
+
+    Assert.That(value.ToString(), Is.EqualTo(expected));
+  }
+}
---
+++ tests/Smdn.Net.EchonetLite.RouteB/Smdn.Net.EchonetLite.RouteB/ElectricEnergyValue.cs
@@ -0,0 +1,129 @@
+// SPDX-FileCopyrightText: 2025 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+using System;
+using NUnit.Framework;
+
+namespace Smdn.Net.EchonetLite.RouteB;
+
+[TestFixture]
+public class ElectricEnergyValueTests {
+  [TestCase(-1)]
+  [TestCase(int.MinValue)]
+  public void Ctor_ArgumentOutOfRange_RawValue(int rawValue)
+  {
+    Assert.That(
+      () => new ElectricEnergyValue(rawValue: rawValue, multiplierToKiloWattHours: 1.0m),
+      Throws
+        .TypeOf<ArgumentOutOfRangeException>()
+        .With.Property(nameof(ArgumentOutOfRangeException.ParamName)).EqualTo("rawValue")
+        .With.Property(nameof(ArgumentOutOfRangeException.ActualValue)).EqualTo(rawValue)
+    );
+  }
+
+  [TestCase(0.0)]
+  [TestCase(-0.0)]
+  [TestCase(-0.1)]
+  [TestCase(-1.0)]
+  public void Ctor_ArgumentOutOfRange_MultiplierToKiloWattHours(decimal multiplierToKiloWattHours)
+  {
+    Assert.That(
+      () => new ElectricEnergyValue(rawValue: 0, multiplierToKiloWattHours: multiplierToKiloWattHours),
+      Throws
+        .TypeOf<ArgumentOutOfRangeException>()
+        .With.Property(nameof(ArgumentOutOfRangeException.ParamName)).EqualTo("multiplierToKiloWattHours")
+        .With.Property(nameof(ArgumentOutOfRangeException.ActualValue)).EqualTo(multiplierToKiloWattHours)
+    );
+  }
+
+  [Test]
+  public void Zero()
+  {
+    Assert.That(ElectricEnergyValue.Zero.RawValue, Is.EqualTo(0));
+    Assert.That(ElectricEnergyValue.Zero.IsValid, Is.True);
+    Assert.That(ElectricEnergyValue.Zero.WattHours, Is.EqualTo(0.0m));
+    Assert.That(ElectricEnergyValue.Zero.KiloWattHours, Is.EqualTo(0.0m));
+    Assert.That(ElectricEnergyValue.Zero.ToString(), Is.EqualTo("0 [kWh]"));
+  }
+
+  [Test]
+  public void NoMeasurementData()
+  {
+    Assert.That(ElectricEnergyValue.NoMeasurementData.RawValue, Is.EqualTo(unchecked((int)0x_FFFF_FFFEu)));
+    Assert.That(ElectricEnergyValue.NoMeasurementData.IsValid, Is.False);
+    Assert.That(ElectricEnergyValue.NoMeasurementData.WattHours, Is.EqualTo(0.0m));
+    Assert.That(ElectricEnergyValue.NoMeasurementData.KiloWattHours, Is.EqualTo(0.0m));
+    Assert.That(ElectricEnergyValue.NoMeasurementData.ToString(), Is.EqualTo("(no data)"));
+  }
+
+  [TestCase(0, 1.0)]
+  [TestCase(0, 10.0)]
+  [TestCase(1, 1.0)]
+  [TestCase(1, 10.0)]
+  [TestCase(unchecked((int)0x_FFFF_FFFEu), 0.0)]
+  [TestCase(unchecked((int)0x_FFFF_FFFEu), 1.0)]
+  [TestCase(unchecked((int)0x_FFFF_FFFEu), 10.0)]
+  public void RawValue(int rawValue, decimal multiplierToKiloWattHours)
+  {
+    var value = new ElectricEnergyValue(
+      rawValue: rawValue,
+      multiplierToKiloWattHours: multiplierToKiloWattHours
+    );
+
+    Assert.That(value.RawValue, Is.EqualTo(rawValue));
+  }
+
+  private static System.Collections.IEnumerable YieldTestCases_KiloWattHours()
+  {
+    yield return new object?[] { 0, 1.0m, 0.0m };
+    yield return new object?[] { 0, 10.0m, 0.0m };
+    yield return new object?[] { 0, 0.1m, 0.0m };
+    yield return new object?[] { 1, 1.0m, 1.0m };
+    yield return new object?[] { 1, 10.0m, 10.0m };
+    yield return new object?[] { 1, 0.1m, 0.1m };
+    yield return new object?[] { 10, 1.0m, 10.0m };
+    yield return new object?[] { 10, 10.0m, 100.0m };
+    yield return new object?[] { 10, 0.1m, 1.0m };
+    yield return new object?[] { 0x05F5E0FF, 1.0m, 99_999_999.0m };
+    yield return new object?[] { 0x05F5E0FF, 10.0m, 999_999_990.0m };
+    yield return new object?[] { 0x05F5E0FF, 0.1m, 9_999_999.9m };
+  }
+
+  [TestCaseSource(nameof(YieldTestCases_KiloWattHours))]
+  public void KiloWattHours(int rawValue, decimal multiplierToKiloWattHours, decimal expected)
+  {
+    var value = new ElectricEnergyValue(
+      rawValue: rawValue,
+      multiplierToKiloWattHours: multiplierToKiloWattHours
+    );
+
+    Assert.That(value.KiloWattHours, Is.EqualTo(expected));
+  }
+
+  private static System.Collections.IEnumerable YieldTestCases_WattHours()
+  {
+    yield return new object?[] { 0, 1.0m, 0.0m };
+    yield return new object?[] { 0, 10.0m, 0.0m };
+    yield return new object?[] { 0, 0.1m, 0.0m };
+    yield return new object?[] { 1, 1.0m, 1_000.0m };
+    yield return new object?[] { 1, 10.0m, 10_000.0m };
+    yield return new object?[] { 1, 0.1m, 100.0m };
+    yield return new object?[] { 10, 1.0m, 10_000.0m };
+    yield return new object?[] { 10, 10.0m, 100_000.0m };
+    yield return new object?[] { 10, 0.1m, 1_000.0m };
+    yield return new object?[] { 0x05F5E0FF, 1.0m, 99_999_999_000.0m };
+    yield return new object?[] { 0x05F5E0FF, 10.0m, 999_999_990_000.0m };
+    yield return new object?[] { 0x05F5E0FF, 0.1m, 9_999_999_900.0m };
+  }
+
+  [TestCaseSource(nameof(YieldTestCases_WattHours))]
+  public void WattHours(int rawValue, decimal multiplierToKiloWattHours, decimal expected)
+  {
+    var value = new ElectricEnergyValue(
+      rawValue: rawValue,
+      multiplierToKiloWattHours: multiplierToKiloWattHours
+    );
+
+    Assert.That(value.WattHours, Is.EqualTo(expected));
+    Assert.That(value.KiloWattHours, Is.EqualTo(expected * 0.001m));
+  }
+}
