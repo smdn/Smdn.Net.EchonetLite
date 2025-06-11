fbdfb84ec14c62d80b5a333aea2667e9edbeebfd
smdn <smdn@smdn.jp>
2025-06-11 23:51:05 +0900

    update examples

--- examples/Smdn.Net.EchonetLite.RouteB/CumulativeElectricEnergyHistoricalData/Program.cs
+++ examples/Smdn.Net.EchonetLite.RouteB/CumulativeElectricEnergyHistoricalData/Program.cs
@@ -3,54 +3,53 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-using Smdn.Devices.BP35XX;
 using Smdn.Net.EchonetLite;
 using Smdn.Net.EchonetLite.RouteB;
-using Smdn.Net.EchonetLite.RouteB.Credentials;
-using Smdn.Net.EchonetLite.RouteB.Transport;
-using Smdn.Net.EchonetLite.RouteB.Transport.BP35XX;
-using Smdn.Net.EchonetLite.RouteB.Transport.SkStackIP;
+using Smdn.Net.EchonetLite.RouteB.DependencyInjection;
 using Smdn.Net.SkStackIP;
 
 // ServiceCollectionを使用して、Bルート設定値やWi-SUNデバイスの追加を行います
 var services = new ServiceCollection();
 
-// Bルート認証情報を追加します
-services.AddRouteBCredential(
-  id: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", // BルートID(ハイフン・スペースなし)を指定してください
-  password: "XXXXXXXXXXXX" // Bルートパスワードを指定してください
-);
+// Bルート関連で使用されるサービスを構築します
+services.AddRouteB(
+  routeBServicesBuilder => {
+    // Bルート認証情報を追加します
+    routeBServicesBuilder.AddCredential(
+      id: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", // BルートID(ハイフン・スペースなし)を指定してください
+      password: "XXXXXXXXXXXX" // Bルートパスワードを指定してください
+    );
 
-// Bルート通信を行うWi-SUNデバイスを追加・設定します
-services.AddRouteBHandler(
-  builder => builder
-    // Bルート通信を行うデバイスとしてBP35A1を追加します
+    // Bルート通信を行うためのハンドラとして、Wi-SUNデバイスBP35A1を使用する実装を追加します
     // (PackageReferenceに`Smdn.Net.EchonetLite.RouteB.BP35XX`を追加してください)
-    .AddBP35A1(
-      static bp35a1 => {
+    routeBServicesBuilder.AddBP35A1Handler(
+      // 使用するBP35A1を設定します
+      configureBP35A1Options: bp35a1Options => {
         // BP35A1とのUART通信を行うためのシリアルポート名を指定してください
         // Windowsでは`COM1`、Linux等では`/dev/ttyACM0`, `/dev/ttyUSB0`といった名前を指定してください
-        bp35a1.SerialPortName = "/dev/ttyACM0";
-      }
-    )
-    // Bルート通信を行うためのPANAセッションを設定します
-    .ConfigureSession(
-      // 特に設定しない場合、自動的にアクティブスキャンを行ってスマートメーターを発見し、セッションを開始します
-      // ただし、アクティブスキャンには数十秒〜数分程度の時間がかかる場合があります
-      // 以下の設定値を設定することで即座にセッションを開始することが可能となるため、
-      // 別の手段で既知である場合は設定しておくことを推奨します
-      session => {
+        bp35a1Options.SerialPortName = "/dev/ttyACM0";
+      },
+      // Bルート通信を行うためのPANAセッションを設定します
+      configureSessionOptions: sessionOptions => {
+        // 特に設定しない場合、自動的にアクティブスキャンを行ってスマートメーターを発見し、セッションを開始します
+        // ただし、アクティブスキャンには数十秒〜数分程度の時間がかかる場合があります
+        // 以下の設定値を設定することで即座にセッションを開始することが可能となるため、
+        // 別の手段で既知である場合は設定しておくことを推奨します
+
         // 接続先のIPアドレスまたはMACアドレスを指定してください
-        // session.PaaAddress = IPAddress.Parse("2001:0db8:0000:0000:0000:0000:0000:0001");
-        // session.PaaMacAddress = PhysicalAddress.Parse("00-00-5E-FF-FE-00-53-00");
+        // sessionOptions.PaaAddress = IPAddress.Parse("2001:0db8:0000:0000:0000:0000:0000:0001");
+        // sessionOptions.PaaMacAddress = PhysicalAddress.Parse("00-00-5E-FF-FE-00-53-00");
 
         // 接続先のPAN IDを指定してください
-        // session.PanId = 0xBEAF;
+        // sessionOptions.PanId = 0xBEAF;
 
         // 接続に使用するチャンネルを指定してください
-        // session.Channel = SkStackChannel.Channels[0x21];
-      }
-    )
+        // sessionOptions.Channel = SkStackChannel.Channels[0x21];
+      },
+      // Bルート通信を行うハンドラを作成するファクトリを構成します
+      configureRouteBHandlerFactory: builder => { }
+    );
+  }
 );
 
 // ログ出力を設定します
